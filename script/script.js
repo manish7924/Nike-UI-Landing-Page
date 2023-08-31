b15c989b6808c40a9ca5dba81fcf0d48d204148e
Manish Chidar <114106502+manish7924@users.noreply.github.com>
2023-08-31 21:48:29 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+console.log('Hello World!');
+var menulist = document.getElementById("menulist");
+menulist.style.maxHeight = "0px";
+
+function togglemenu() {
+  if (menulist.style.maxHeight == "0px")
+  {
+    menulist.style.maxHeight = "130px"
+  }
+  else
+  {
+    menulist.style.maxHeight = "0px"
+
+  }
+}
